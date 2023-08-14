55785d775888f824e0d1530586dcebbb7c76b899
ScottHull <scottdhull21@gmail.com>
2023-08-14 01:26:10 -0400

    Still in development

--- src/vectorized_elements.py
+++ src/vectorized_elements.py
@@ -10,7 +10,7 @@ def angular_momentum_vector(df: pd.DataFrame):
     """
     position = df[['x', 'y', 'z']].values
     velocity = df[['vx', 'vy', 'vz']].values
-    return df['mass'] * np.cross(position, velocity)
+    return df['mass'].tolist() * np.cross(position, velocity)
     # return [mass * np.cross(p, v) for mass, p, v in zip(df['mass'], position, velocity)]
 
 

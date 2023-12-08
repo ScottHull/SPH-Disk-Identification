05662ef2733d377bac22ed28904b83afa746043f
ScottHull <scottdhull21@gmail.com>
2023-12-08 13:05:41 -0500

    added plot_disk.py

--- plot_disk.py
+++ plot_disk.py
@@ -134,8 +134,8 @@ for index, a in enumerate(ax.flatten()):
 
 ax[0, 0].annotate(r"x ($10^4$ km)", xy=(0.0, -5.5), ha="center", fontsize=16, weight='bold')
 ax[0, 0].annotate(r"y ($10^4$ km)", xy=(-5.5, 0.0), va="center", rotation=90, fontsize=16, weight='bold')
-ax[0, 0].text(0.50, 0.05, r"x ($10^4$ km)", transform=ax.transAxes, va="center", fontsize=14, weight='bold')
-ax[0, 0].text(0.05, 0.5, r"y ($10^4$ km)", transform=ax.transAxes, va="center", rotation=90, fontsize=14, weight='bold')
+ax[0, 0].text(0.50, 0.05, r"x ($10^4$ km)", transform=ax[0, 0].transAxes, va="center", fontsize=14, weight='bold')
+ax[0, 0].text(0.05, 0.5, r"y ($10^4$ km)", transform=ax[0, 0].transAxes, va="center", rotation=90, fontsize=14, weight='bold')
 # plt.tight_layout()
 fig.subplots_adjust(wspace=0, hspace=0)
 axs = ax.flatten()

0b6685dac88f3abf4650938828cf28982e707328
ScottHull <scottdhull21@gmail.com>
2023-12-08 19:17:10 -0500

    added plot_disk.py

--- plot_disk.py
+++ plot_disk.py
@@ -137,7 +137,10 @@ ax[0, 0].annotate(r"y ($10^4$ km)", xy=(-5.5, 0.0), va="center", rotation=90, fo
 ax[0, 0].text(0.50, 0.05, r"x ($10^4$ km)", transform=ax[0, 0].transAxes, ha="center", fontsize=14, weight='bold')
 ax[0, 0].text(0.05, 0.5, r"y ($10^4$ km)", transform=ax[0, 0].transAxes, va="center", rotation=90, fontsize=14, weight='bold')
 # plt.tight_layout()
-fig.subplots_adjust(wspace=0, hspace=0)
+# fig.subplots_adjust(wspace=0, hspace=0)
+plt.subplots_adjust(top = 1, bottom = 0, right = 1, left = 0,
+            hspace = 0, wspace = 0)
+plt.margins(0,0)
 axs = ax.flatten()
 for ax in axs[-len(runs):-2]:
     nbins_x = len(ax.get_xticklabels())

Fixes to VMF calculation in report.py

src/report.py
@@ -9,7 +9,7 @@ class GiantImpactReport:
     This class is responsible for generating a report of the giant impact simulation.
     """
 
-    def calculate_vmf(self, particles: pd.DataFrame, phase_curve: pd.DataFrame):
+    def calculate_vmf(self, particles: pd.DataFrame, phase_curve: pd.DataFrame, entropy_col='total entropy'):
         """
         Calculates the vapor mass fraction (vmf) of the particles.
         Assumes single phase particles.
@@ -21,7 +21,7 @@ class GiantImpactReport:
         # if the total entropy column is not defined, raise an error stating that it is required
         # ideally, total entropy should include both the particle entropy and the entropy gain associated with the
         # orbital circularization
-        if 'total entropy' not in particles.columns:
+        if 'total entropy' not in particles.columns and entropy_col == 'total entropy':
             raise AttributeError("A 'total entropy' column is required to calculate the velocity mass fraction.\n"
                                  "This is the sum of the particle entropy and the entropy "
                                  "gain associated with orbital circularization.")
@@ -45,18 +45,19 @@ class GiantImpactReport:
         # define rules on whether the particle is pure liquid or pure vapor, or supercritical or mixed phase
         is_supercritical = particles['temperature'] >= supercritical_T
 
-        # calculate vmf with effects of orbital circularization
-        is_pure_liquid_w_circ = particles['total entropy'] <= particles['nearest liquid entropy']
-        is_pure_vapor_w_circ = particles['total entropy'] >= particles['nearest vapor entropy']
-        # calculate the vmf for each particle
-        particles['vmf_w_circ'] = np.where(
-            is_supercritical, 1.0, np.where(
-                is_pure_liquid_w_circ, 0.0, np.where(
-                    is_pure_vapor_w_circ, 1.0, (particles['total entropy'] - particles['nearest liquid entropy']) / (
-                            particles['nearest vapor entropy'] - particles['nearest liquid entropy'])
+        if entropy_col == 'total entropy':
+            # calculate vmf with effects of orbital circularization
+            is_pure_liquid_w_circ = particles['total entropy'] <= particles['nearest liquid entropy']
+            is_pure_vapor_w_circ = particles['total entropy'] >= particles['nearest vapor entropy']
+            # calculate the vmf for each particle
+            particles['vmf_w_circ'] = np.where(
+                is_supercritical, 1.0, np.where(
+                    is_pure_liquid_w_circ, 0.0, np.where(
+                        is_pure_vapor_w_circ, 1.0, (particles['total entropy'] - particles['nearest liquid entropy']) / (
+                                particles['nearest vapor entropy'] - particles['nearest liquid entropy'])
+                    )
                 )
             )
-        )
 
         is_pure_liquid_wo_circ = particles['entropy'] <= particles['nearest liquid entropy']
         is_pure_vapor_wo_circ = particles['entropy'] >= particles['nearest vapor entropy']
@@ -69,6 +70,9 @@ class GiantImpactReport:
                 )
             )
         )
+
+        if entropy_col != 'total entropy':
+            particles['vmf_w_circ'] = particles['vmf_wo_circ']
 
         return [
             particles['vmf_w_circ'].sum() / len(particles) * 100,
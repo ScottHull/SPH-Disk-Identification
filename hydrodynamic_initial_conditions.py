7ec84bb8b944057818fa5c87e81ecccc5baf68fa
Scott Hull <ScottHull@users.noreply.github.com>
2023-12-10 11:53:30 -0500

    Fixes to VMF calculation in report.py

--- hydrodynamic_initial_conditions.py
+++ hydrodynamic_initial_conditions.py
@@ -107,7 +107,10 @@ for index, run in enumerate(runs):
     )
     axs[1, index].set_xlabel("Time (hrs.)", fontsize=18)
     axs[1, index].set_ylabel("Avg. Ejecta Velocity (km/s)", fontsize=18)
-    axs[1, index].axvline(max_iteration, color='black', linestyle='--')
+    axs[1, index].axvline(run['times'][max_iteration], color='black', linestyle='--')
+    axs[1, index].text(
+        0.6, 0.9, r"$t_{\rm ic} = $" + f"{run['times'][max_iteration]} hrs.", transform=axs[index].transAxes, size=20
+    )
     axs[2, index].plot(
         run['times'], run['temperatures'], linewidth=2.0, color='blue'
     )
@@ -115,6 +118,7 @@ for index, run in enumerate(runs):
     axs[2, index].plot(
         run['times'], run['vmfs'], linewidth=2.0, color='red'
     )
+    axs[2, index].axvline(run['times'][max_iteration], color='black', linestyle='--')
     axs[2, index].set_xlabel("Time (hrs.)", fontsize=18)
     axs[2, index].set_ylabel("Avg. Disk Temperature (K)", fontsize=18)
     ax2.set_ylabel("Disk VMF (%)", fontsize=18)

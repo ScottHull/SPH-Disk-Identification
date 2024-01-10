Fixes to VMF calculation in report.py

track_disk.py
@@ -81,8 +81,8 @@ file_headers = ["id", "tag", "mass", "x", "y", "z", "vx", "vy", "vz", "density",
                 "potential energy", "entropy", "temperature"]
 axes = ['times', 'disk_entropy_w_circ', 'disk_temperature', 'disk_vmf_w_circ', 'disk_mass', 'disk_angular_momentum',
         'disk_impactor_mass_fraction', 'disk_vmf_wo_circ', 'disk_entropy_wo_circ']
-ylabels = ["Avg. Disk Entropy (J/kg/K)", "Avg. Disk Temperature (K)", "Disk VMF (%)", r"Disk Mass ($M_{\rm PD}$)",
-             r"Disk Angular Momentum ($L_{\rm MM}$)", "Disk Impactor Mass Fraction (%)"]
+ylabels = ["Avg. Disk Entropy (J/kg/K)", "Avg. Disk Temperature (K)", "Disk VMF (%)", r"Disk Mass ($10^3$ $M_{\rm PD}$)",
+             r"$L_{\rm disk}^*$", "$f_{\rm disk}$ (%)"]
 phase_curve = pd.read_fwf("src/phase_curves/forstSTS__vapour_curve.txt", skiprows=1,
                            names=["temperature", "density_sol_liq", "density_vap", "pressure",
                                   "entropy_sol_liq", "entropy_vap"])
@@ -108,17 +108,20 @@ for run in runs:
         particles = particle_map.loop()
         disk_particles = particles[particles['label'] == 'DISK']
         if len(disk_particles) > 0:
-            disk_mass = disk_particles[disk_particles['tag'] > 1]['mass'].sum() / (mass_phobos + mass_deimos)
+            disk_mass = disk_particles[disk_particles['tag'] % 2 == 0]['mass'].sum() / (1000 * (mass_phobos + mass_deimos))
             disk_ang_mom = disk_particles['angular momentum'].sum()
-            disk_impactor_mass_fraction = disk_particles[disk_particles['tag'] > 1]['mass'].sum() / disk_particles['mass'].sum() * 100
+            disk_impactor_mass_fraction = disk_particles[disk_particles['tag'] > 1]
+            disk_impactor_mass_fraction = disk_impactor_mass_fraction[disk_impactor_mass_fraction['tag'] % 2 != 0]['mass'].sum() / disk_particles['mass'].sum() * 100
         else:
             disk_mass = 0.0
             disk_ang_mom = 0.0
             disk_impactor_mass_fraction = 0.0
+        # scale the disk angular momentum
+        L_scaled = disk_ang_mom / (disk_mass * np.sqrt((6.67 * 10 ** -11) * mass_planet * 2.5 * equatorial_radius))
         disk_vmf_w_circ, disk_vmf_wo_circ = dr.calculate_vmf(disk_particles, phase_curve)
         run['times'].append(time)
         run['disk_mass'].append(disk_mass)
-        run['disk_angular_momentum'].append(disk_ang_mom)
+        run['disk_angular_momentum'].append(L_scaled)
         run['disk_entropy_w_circ'].append(disk_particles['total entropy'].mean())
         run['disk_entropy_wo_circ'].append(disk_particles['entropy'].mean())
         run['disk_impactor_mass_fraction'].append(disk_impactor_mass_fraction)
@@ -128,6 +131,8 @@ for run in runs:
 
 fig, axs = plt.subplots(2, 3, figsize=(18, 9), sharex='all')
 axs = axs.flatten()
+axs[1].set_ylim(1500, 3000)
+axs[2].set_ylim(0, 30)
 for ax, (axis, ylabel) in zip(axs, zip(axes[1:-2], ylabels)):
     for index, run in enumerate(runs):
         ax.plot(run['times'], run[axis], linewidth=2.0, color=colors[index], label=run['name'])
@@ -137,12 +142,12 @@ for ax, (axis, ylabel) in zip(axs, zip(axes[1:-2], ylabels)):
     # use 16 point font on the axes
     ax.tick_params(axis='both', which='major', labelsize=16)
 for index, run in enumerate(runs):
-    axs[0].plot(run['times'], run['disk_entropy_wo_circ'], linewidth=2.0, color=colors[index], linestyle="--", label=run['name'])
-    axs[2].plot(run['times'], run['disk_vmf_wo_circ'], linewidth=2.0, color=colors[index], linestyle="--", label=run['name'])
+    axs[0].plot(run['times'], run['disk_entropy_wo_circ'], linewidth=2.0, color=colors[index], linestyle="--")
+    axs[2].plot(run['times'], run['disk_vmf_wo_circ'], linewidth=2.0, color=colors[index], linestyle="--")
 letters = string.ascii_lowercase
 # annotate the plots with letters in the upper left corner
 for ax, letter in zip(axs, letters):
-    ax.text(0.95, 0.95, f"{letter}", transform=ax.transAxes, fontsize=16, fontweight='bold', va='top')
+    ax.text(0.90, 0.95, f"{letter}", transform=ax.transAxes, fontsize=16, fontweight='bold', va='top')
 axs[0].legend(fontsize=16)
 plt.tight_layout()
 plt.savefig(f"disk_profile.png", dpi=200)